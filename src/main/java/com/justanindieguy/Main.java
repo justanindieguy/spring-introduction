59c95fb4906d4f5343f8e4418c52260054092c22
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-11 14:25:09 -0600

    close context to invoke methods PreDestroy annotated methods

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -9,9 +9,12 @@ public class Main {
 
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
     Vehicle vehicle = context.getBean(Vehicle.class);
     System.out.println("Component Vehicle name from Spring Context is: " + vehicle.getName());
     vehicle.printHello();
+
+    context.close();
   }
 
 }
