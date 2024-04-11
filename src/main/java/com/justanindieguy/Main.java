8e4fde3e31b323f05ab7bb888aeea2a1584700e7
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-11 11:50:59 -0600

    update names when fetching vehicles from Spring context to match new assigned names

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -19,11 +19,11 @@ public class Main {
      */
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-    Vehicle vehicle1 = context.getBean("vehicle1", Vehicle.class);
+    Vehicle vehicle1 = context.getBean("audiVehicle", Vehicle.class);
     System.out.println("Vehicle1 name from Spring context is: " + vehicle1.getName());
-    Vehicle vehicle2 = context.getBean("vehicle2", Vehicle.class);
+    Vehicle vehicle2 = context.getBean("hondaVehicle", Vehicle.class);
     System.out.println("Vehicle2 name from Spring context is: " + vehicle2.getName());
-    Vehicle vehicle3 = context.getBean("vehicle3", Vehicle.class);
+    Vehicle vehicle3 = context.getBean("ferrariVehicle", Vehicle.class);
     System.out.println("Vehicle3 name from Spring context is: " + vehicle3.getName());
 
     /*
