d1467d83fdef8bcbcd2ee257f8e8e870602ffbd7
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-12 12:38:17 -0600

    start Spring Context providing XML configuration

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -1,8 +1,15 @@
 package com.justanindieguy;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.justanindieguy.beans.Vehicle;
+
 public class Main {
 
   public static void main(String[] args) {
+    var context = new ClassPathXmlApplicationContext("beans.xml");
+    Vehicle vehicle = context.getBean(Vehicle.class);
+    System.out.println("Vehicle name from Spring context is: " + vehicle.getName());
   }
 
 }
