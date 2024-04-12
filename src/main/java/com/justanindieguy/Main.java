a007561cc5e46846668422010e084fa557537ff1
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-12 14:21:54 -0600

    get person and vehicle beans from context, check the relationship between them

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -2,6 +2,7 @@ package com.justanindieguy;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.justanindieguy.beans.Person;
 import com.justanindieguy.beans.Vehicle;
 import com.justanindieguy.config.ProjectConfig;
 
@@ -10,8 +11,11 @@ public class Main {
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+    Person person = context.getBean(Person.class);
     Vehicle vehicle = context.getBean(Vehicle.class);
+    System.out.println("Person name from Spring Context is: " + person.getName());
     System.out.println("Vehicle name from Spring context is: " + vehicle.getName());
+    System.out.println("Vehicle that Person owns is: " + person.getVehicle());
   }
 
 }
