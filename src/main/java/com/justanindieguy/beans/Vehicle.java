3468765b3ea5d4d137b1a827c60107adb6e45e65
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 10:36:14 -0600

    mark Vehicle POJO java class as a Spring component using @Component annotation, set initial value to name property using @PostConstruct annotation

--- src/main/java/com/justanindieguy/beans/Vehicle.java
+++ src/main/java/com/justanindieguy/beans/Vehicle.java
@@ -1,10 +1,20 @@
 package com.justanindieguy.beans;
 
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
 public class Vehicle {
   private String name;
 
   public Vehicle() {
     System.out.println("Vehicle bean created by Spring!");
+  }
+
+  @PostConstruct
+  public void initialize() {
+    name = "Honda";
   }
 
   public String getName() {
