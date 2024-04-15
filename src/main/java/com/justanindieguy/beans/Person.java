c7d3804581a2d2dcfbc8cd5f3513de57a33800c5
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 10:36:55 -0600

    mark Person POJO java class as a Spring component using @Component annotation, initialize name value using @PostConstruct, specify a dependency with Vehicle Bean using @Autowired

--- src/main/java/com/justanindieguy/beans/Person.java
+++ src/main/java/com/justanindieguy/beans/Person.java
@@ -1,11 +1,29 @@
 package com.justanindieguy.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
 public class Person {
   private String name;
+
+  /*
+   * The @Autowired annotation marks on a field, constructor, Setter method
+   * is used to auto-wire the beans that is 'injecting beans' (Objects) at
+   * runtime by Spring Dependency Injection mechanism
+   */
+  @Autowired
   private Vehicle vehicle;
 
   public Person() {
     System.out.println("Person bean created by Spring!");
+  }
+
+  @PostConstruct
+  public void initialize() {
+    name = "John";
   }
 
   public String getName() {
