a0b7f90c587274bafbb95c79e2a4fec7e8b919a7
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 13:58:38 -0600

    create context, remove attempt to get vehicle since there many beans of same type

--- src/main/java/com/justanindieguy/Main.java
+++ src/main/java/com/justanindieguy/Main.java
@@ -3,7 +3,6 @@ package com.justanindieguy;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.justanindieguy.beans.Person;
-import com.justanindieguy.beans.Vehicle;
 import com.justanindieguy.config.ProjectConfig;
 
 public class Main {
@@ -11,9 +10,7 @@ public class Main {
   public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
     Person person = context.getBean(Person.class);
-    Vehicle vehicle = context.getBean(Vehicle.class);
     System.out.println("Person name from Spring Context is: " + person.getName());
-    System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
     System.out.println("Vehicle that Person owns is: " + person.getVehicle());
   }
 
