9d2f369345309a27210897a68d7330cda0e5c925
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 14:06:21 -0600

    create multiple Beans of same type Computer, specify one as the Primary bean

--- src/main/java/com/justanindieguy/config/ProjectConfig.java
+++ src/main/java/com/justanindieguy/config/ProjectConfig.java
@@ -3,7 +3,9 @@ package com.justanindieguy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import com.justanindieguy.beans.Computer;
 import com.justanindieguy.beans.Vehicle;
 
 @Configuration
@@ -29,6 +31,21 @@ public class ProjectConfig {
     var vehicle = new Vehicle();
     vehicle.setName("Ferrari");
     return vehicle;
+  }
+
+  @Bean
+  Computer computer1() {
+    var computer = new Computer();
+    computer.setName("Linux Laptop");
+    return computer;
+  }
+
+  @Bean
+  @Primary
+  Computer computer2() {
+    var computer = new Computer();
+    computer.setName("Windows Laptop");
+    return computer;
   }
 
 }
