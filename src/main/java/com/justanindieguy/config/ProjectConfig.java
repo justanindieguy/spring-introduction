eb74812e623836ddd074e44d00e8e7d09f1dd068
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 10:24:06 -0600

    add @ComponentScan and base packages to scan for beans

--- src/main/java/com/justanindieguy/config/ProjectConfig.java
+++ src/main/java/com/justanindieguy/config/ProjectConfig.java
@@ -1,7 +1,9 @@
 package com.justanindieguy.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.justanindieguy.beans")
 public class ProjectConfig {
 }
