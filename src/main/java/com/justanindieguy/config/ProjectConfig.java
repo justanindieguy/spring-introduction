2ccebcd5c18bfda808de6822aea7af880ded3e76
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 09:30:46 -0600

    remove unnecessary code from ProjectConfig

--- src/main/java/com/justanindieguy/config/ProjectConfig.java
+++ src/main/java/com/justanindieguy/config/ProjectConfig.java
@@ -1,62 +1,7 @@
 package com.justanindieguy.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import com.justanindieguy.beans.Vehicle;
-
-/*
- * Spring @Configuration annotation is part of the spring core framework.
- * Spring Configuration annotation indicates that the class has @Bean definition
- * methods. So Spring container can process the class and generate Spring Beans
- * to be used in the application.
- */
 @Configuration
 public class ProjectConfig {
-
-  /*
-   * @Bean annotation, which lets Spring know that it needs to call
-   * this method when it initializes its context and adds the returned
-   * value to the context.
-   */
-  @Bean(name = "audiVehicle")
-  Vehicle vehicle1() {
-    var veh = new Vehicle();
-    veh.setName("Audi");
-    return veh;
-  }
-
-  @Primary
-  @Bean(name = "hondaVehicle")
-  Vehicle vehicle2() {
-    var veh = new Vehicle();
-    veh.setName("Honda");
-    return veh;
-  }
-
-  // @Primary <- throws an exception, two Primary annotations for same type
-  @Bean("ferrariVehicle")
-  Vehicle vehicle3() {
-    var veh = new Vehicle();
-    veh.setName("Ferrari");
-    return veh;
-  }
-
-  /*
-   * The method names usually follow verbs notation. But for methods
-   * which we will use to create beans, can use nouns as names.
-   * This will be a good practice as the method names will become
-   * bean names as well in the context.
-   */
-  @Bean
-  String hello() {
-    return "Hello, World!";
-  }
-
-  @Bean
-  Integer number() {
-    return 16;
-  }
-
 }
