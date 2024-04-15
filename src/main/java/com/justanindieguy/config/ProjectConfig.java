931a3a8037f9d2d3e3791c35ab9816d1d59c56da
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-15 13:30:56 -0600

    create multiple Beans of same Vehicle class type in ProjectConfig

--- src/main/java/com/justanindieguy/config/ProjectConfig.java
+++ src/main/java/com/justanindieguy/config/ProjectConfig.java
@@ -1,9 +1,34 @@
 package com.justanindieguy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.justanindieguy.beans.Vehicle;
 
 @Configuration
 @ComponentScan(basePackages = "com.justanindieguy.beans")
 public class ProjectConfig {
+
+  @Bean
+  Vehicle vehicle1() {
+    var vehicle = new Vehicle();
+    vehicle.setName("Audi");
+    return vehicle;
+  }
+
+  @Bean
+  Vehicle vehicle2() {
+    var vehicle = new Vehicle();
+    vehicle.setName("Honda");
+    return vehicle;
+  }
+
+  @Bean
+  Vehicle vehicle3() {
+    var vehicle = new Vehicle();
+    vehicle.setName("Ferrari");
+    return vehicle;
+  }
+
 }
